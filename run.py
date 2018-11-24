782ce7edddd003b6b60044b0a074e9493e581f18
Lukas Hudec <hudec.lukas21@gmail.com>
2018-11-24 23:16:32 +0100

    merge

--- run.py
+++ run.py
@@ -8,11 +8,13 @@ import tensorflow as tf
 from models.model_GAVAE_SIM_tf import GAVAE_SIM
 
 def main(_):
-    DATA_PATH = 'D:/Vision_Images/Pexels_textures/Textures/official'
-    tex_gavae = GAVAE_SIM(data_path=DATA_PATH, w=160, h=160, c=1, layer_depth=3, batch_size=4)
-    tex_gavae.train(epochs=50000, save_interval=10, model_file='test.h5')
+    DATA_PATH = 'D:\\HudecL\\Pexels\\Textures\\official'
+    tex_gavae = GAVAE_SIM(data_path=DATA_PATH, w=160, h=160, c=1, layer_depth=3, batch_size=8)
     # tex_gavae.test_discriminator(model_file='test.h5',path='images')
+    tex_gavae.train(epochs=50000, save_interval=10, model_file='test.h5')
+
     return 0
+
 
 if __name__ == '__main__':
     tf.app.run()
