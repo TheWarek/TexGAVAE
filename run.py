6b7e1362905e95bd2e73faeb780c4a23a50f984f
Marek <marko.jakab@gmail.com>
2018-12-13 13:44:50 +0100

    [CODE] fix merge

--- run.py
+++ run.py
@@ -5,12 +5,13 @@ from __future__ import print_function
 
 import tensorflow as tf
 # from models.model_GAVAE_SIM import GAVAE_SIM
-from models.model_GAVAE_SIM_tf import GAVAE_SIM
+# from models.model_GAVAE_SIM_tf import GAVAE_SIM
+from models.model_GAVAE_SIM_tfk_disc import GAVAE_SIM
 
 def main(_):
     DATA_PATH = 'D:/Vision_Images/Pexels_textures/Textures/official'
     tex_gavae = GAVAE_SIM(data_path=DATA_PATH, w=160, h=160, c=1, layer_depth=3, batch_size=8)
-    tex_gavae.train(epochs=50000, save_interval=200, log_interval=50, model_file='test.h5')
+    tex_gavae.train(epochs=50000, save_interval=200, model_file='test.h5')
     # tex_gavae.test(model_file='test.h5')
     # tex_gavae.test_discriminator(model_file='test.h5',path='images')
     return 0
