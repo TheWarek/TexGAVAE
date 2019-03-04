af5c80036967553bc9ea5544c4a2f52acdc18e82
Lukas Hudec <hudec.lukas21@gmail.com>
2019-03-04 01:53:38 +0100

    [VAE 2.0] new ladder VAE 2.0 architecture with stacked training

--- models/model_vae20.py
+++ models/model_vae20.py
@@ -21,7 +21,7 @@ class vae_next_gen:
         self.test_phase = False
         self.learning_rate = lr
 
-        self.n_z = 20
+        self.n_z = 10
         self.z_1_input = tf.placeholder(dtype=tf.float32, shape=[None, self.n_z], name="z_1_input")
         self.z_2_input = tf.placeholder(dtype=tf.float32, shape=[None, self.n_z], name="z_2_input")
         self.z_3_input = tf.placeholder(dtype=tf.float32, shape=[None, self.n_z], name="z_3_input")
@@ -47,7 +47,7 @@ class vae_next_gen:
             self.texdat = TexDAT(data_path, self.batch_size)
             self.texdat.load_images(False)
 
-        self.dataset = tf.data.Dataset.from_generator(self.dataset_generator, output_types=tf.float32, output_shapes=([None, w, h, c]))
+        self.dataset = tf.data.Dataset.from_generator(self.dataset_generator, output_types=tf.float32, output_shapes=([None, w, h, c]), args=[2048])
         self.dataset_iterator = self.dataset.make_initializable_iterator()
         self.dataset_batch = self.dataset_iterator.get_next()
 
@@ -91,6 +91,12 @@ class vae_next_gen:
                 global_step=self.vae_global_step_4,
                 var_list=self.enc_l4+self.dec_l4
             )
+            self.vae_optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+            self.vae_global_step = tf.Variable(initial_value=0, name='vae_global_step_4', trainable=False)
+            self.vae_train_step = self.vae_optimizer.minimize(
+                loss=self.vae_loss,
+                global_step=self.vae_global_step
+            )
 
 
     def __vae_loss(self, vae_input):
@@ -110,17 +116,43 @@ class vae_next_gen:
             self.vae_summaries.append(tf.summary.scalar('kl_loss_4', tf.reduce_mean(kl_loss_4)))
             # return the average loss over all images in batch
             # kokot neotacaj ho, nechaj +, lebo uz je vynasobeny -1 on sam
-            total_loss = tf.reduce_mean(reconstruction_loss + 2.*kl_loss_1+2.*kl_loss_2+2.*kl_loss_3+2.*kl_loss_4)
+            total_loss = tf.reduce_mean(reconstruction_loss + 4.*kl_loss_1+3.*kl_loss_2+2.*kl_loss_3+1.2*kl_loss_4)
             self.vae_summaries.append(tf.summary.scalar('total_loss', total_loss))
         return total_loss
 
-    def dataset_generator(self):
+    def dataset_generator(self, samples):
         if self.is_textdat:
             sorted_list = self.texdat.train.images
             # indices = np.random.choice(np.arange(len(sorted_list)), size=4, replace=False)
             # indices = [294, 309, 314, 248]
             # indices = [55, 53]
-            indices = [0]
+            indices = np.arange(len(sorted_list))
+
+            if samples > 0:
+                dataset = []
+                for ind in indices:
+                    for i in range(samples):
+                        dataset.append(self.texdat.load_image_patch(sorted_list[ind], patch_size=self.patch_size).reshape(self.patch_size))
+                dataset = normalize_batch_images(dataset, normalize='zeromean')
+                start = 0
+                end = self.batch_size
+                epochs = 0
+                while True:
+                    batch = dataset[start:end]
+                    batch = resize_batch_images(batch, self.patch_size)
+                    start = end
+                    end = end + self.batch_size
+                    self.new_batch = False
+                    while not self.new_batch:
+                        yield batch
+                    if end > len(dataset):
+                        start = 0
+                        end = self.batch_size
+                        epochs+=1
+                        print("End of epoch: ", epochs)
+                        print("Shuffling dataset")
+                        np.random.shuffle(dataset)
+
             if self.test_phase:
                 indices = np.arange(len(sorted_list))
             div = int(self.batch_size / len(indices))
@@ -186,7 +218,7 @@ class vae_next_gen:
                                      reuse=tf.AUTO_REUSE, name='conv_1')
             net_1 = tf.nn.leaky_relu(net_1, name='relu_1')
             net_1 = tf.layers.dropout(net_1, self.drop_rate, name='dropout_1')
-            to_dense_1 = tf.layers.conv2d(net_1, filters=32, kernel_size=1, strides=1,
+            to_dense_1 = tf.layers.conv2d(net_1, filters=2, kernel_size=1, strides=1,
                                      padding='same', reuse=tf.AUTO_REUSE, name='conv1x1_1')
             net_1 = tf.layers.batch_normalization(net_1, name='batchnorm_1')
             net_1 = tf.layers.max_pooling2d(net_1, 2, 2, padding='same', name='maxpool_1')
@@ -199,7 +231,7 @@ class vae_next_gen:
                                      reuse=tf.AUTO_REUSE, name='conv_2')
             net_2 = tf.nn.leaky_relu(net_2, name='relu_2')
             net_2 = tf.layers.dropout(net_2, self.drop_rate, name='dropout_2')
-            to_dense_2 = tf.layers.conv2d(net_2, filters=32, kernel_size=1, strides=1,
+            to_dense_2 = tf.layers.conv2d(net_2, filters=2, kernel_size=1, strides=1,
                                      padding='same', reuse=tf.AUTO_REUSE, name='conv1x1_2')
             net_2 = tf.layers.batch_normalization(net_2, name='batchnorm_2')
             net_2 = tf.layers.max_pooling2d(net_2, 2, 2, padding='same', name='maxpool_2')
@@ -212,7 +244,7 @@ class vae_next_gen:
                                      reuse=tf.AUTO_REUSE, name='conv_3')
             net_3 = tf.nn.leaky_relu(net_3, name='relu_3')
             net_3 = tf.layers.dropout(net_3, self.drop_rate, name='dropout_3')
-            to_dense_3 = tf.layers.conv2d(net_3, filters=64, kernel_size=1, strides=1,
+            to_dense_3 = tf.layers.conv2d(net_3, filters=4, kernel_size=1, strides=1,
                                           padding='same', reuse=tf.AUTO_REUSE, name='conv1x1_3')
             net_3 = tf.layers.batch_normalization(net_3, name='batchnorm_3')
             net_3 = tf.layers.max_pooling2d(net_3, 2, 2, padding='same', name='maxpool_3')
@@ -225,7 +257,7 @@ class vae_next_gen:
                                      reuse=tf.AUTO_REUSE, name='conv_2')
             net_4 = tf.nn.leaky_relu(net_4, name='relu_2')
             net_4 = tf.layers.dropout(net_4, self.drop_rate, name='dropout_2')
-            to_dense_4 = tf.layers.conv2d(net_4, filters=64, kernel_size=1, strides=1,
+            to_dense_4 = tf.layers.conv2d(net_4, filters=4, kernel_size=1, strides=1,
                                           padding='same', reuse=tf.AUTO_REUSE, name='conv1x1_4')
 
         self.enc_l4 = tf.global_variables()
@@ -277,7 +309,7 @@ class vae_next_gen:
         with tf.variable_scope('lowest_layer'):
             out_4 = tf.layers.dense(z4, self.mid_shape[1]*self.mid_shape[2],reuse=tf.AUTO_REUSE, name='squaring_dense_4')
             out_4 = tf.reshape(out_4, (-1, self.mid_shape[1], self.mid_shape[2], 1))
-            net_4 = tf.layers.conv2d(out_4, 64, 3, 1, padding='same', reuse=tf.AUTO_REUSE, name='out_conv_4')
+            net_4 = tf.layers.conv2d(out_4, 32, 3, 1, padding='same', reuse=tf.AUTO_REUSE, name='out_conv_4')
             net_4 = tf.nn.leaky_relu(net_4, name='relu_4')
             net_4 = tf.layers.batch_normalization(net_4, name='batchnorm_4')
             net_4 = tf.layers.dropout(net_4, 'dropout_4')
@@ -288,7 +320,7 @@ class vae_next_gen:
         with tf.variable_scope('second_lowest_layer'):
             out_3 = tf.layers.dense(z3, 2*self.mid_shape[1] * 2*self.mid_shape[2], reuse=tf.AUTO_REUSE, name='squaring_dense_3')
             out_3 = tf.reshape(out_3, (-1, 2*self.mid_shape[1], 2*self.mid_shape[2], 1))
-            out_3 = tf.layers.conv2d(out_3, 64, 3, 1, padding='same',reuse=tf.AUTO_REUSE, name='out_conv_3')
+            out_3 = tf.layers.conv2d(out_3, 32, 3, 1, padding='same',reuse=tf.AUTO_REUSE, name='out_conv_3')
             out_3 = tf.nn.leaky_relu(out_3, name='out_relu_3')
 
             net_3 = tf.image.resize_images(net_4, size=(2 * self.mid_shape[1], 2 * self.mid_shape[2]))
@@ -304,12 +336,12 @@ class vae_next_gen:
         with tf.variable_scope('second_highest_layer'):
             out_2 = tf.layers.dense(z2, 4*self.mid_shape[1] * 4*self.mid_shape[2], reuse=tf.AUTO_REUSE, name='squaring_dense_2')
             out_2 = tf.reshape(out_2, (-1, 4*self.mid_shape[1], 4*self.mid_shape[2], 1))
-            out_2 = tf.layers.conv2d(out_2, 64, 3, 1, padding='same',reuse=tf.AUTO_REUSE, name='out_conv_2')
+            out_2 = tf.layers.conv2d(out_2, 64, 5, 1, padding='same',reuse=tf.AUTO_REUSE, name='out_conv_2')
             out_2 = tf.nn.leaky_relu(out_2, name='out_relu_2')
 
             net_2 = tf.image.resize_images(net_3, size=(4 * self.mid_shape[1], 4 * self.mid_shape[2]))
             net_2 = tf.add(net_2, out_2, name='merge_out_net_2')
-            net_2 = tf.layers.conv2d(net_2, 32, 3, 1, padding='same', reuse=tf.AUTO_REUSE, name='conv_2')
+            net_2 = tf.layers.conv2d(net_2, 64, 5, 1, padding='same', reuse=tf.AUTO_REUSE, name='conv_2')
             net_2 = tf.nn.leaky_relu(net_2, name='relu_2')
             net_2 = tf.layers.batch_normalization(net_2, name='batchnorm_2')
             net_2 = tf.layers.dropout(net_2, 'dropout_2')
@@ -320,16 +352,17 @@ class vae_next_gen:
         with tf.variable_scope('top_layer'):
             out_1 = tf.layers.dense(z1, 8*self.mid_shape[1] * 8*self.mid_shape[2], reuse=tf.AUTO_REUSE, name='squaring_dense_1')
             out_1 = tf.reshape(out_1, (-1, 8*self.mid_shape[1], 8*self.mid_shape[2], 1))
-            out_1 = tf.layers.conv2d(out_1, 32, 3, 1, padding='same',reuse=tf.AUTO_REUSE, name='out_conv_1')
+            out_1 = tf.layers.conv2d(out_1, 64, 5, 1, padding='same',reuse=tf.AUTO_REUSE, name='out_conv_1')
             out_1 = tf.nn.leaky_relu(out_1, name='out_relu_1')
 
             net_1 = tf.image.resize_images(net_2, size=(8 * self.mid_shape[1], 8 * self.mid_shape[2]))
             net_1 = tf.add(net_1, out_1, name='merge_out_net_1')
-            net_1 = tf.layers.conv2d(net_1, 32, 3, 1, padding='same', reuse=tf.AUTO_REUSE, name='conv_1')
+            net_1 = tf.layers.conv2d(net_1, 64, 5, 1, padding='same', reuse=tf.AUTO_REUSE, name='conv_1')
             net_1 = tf.nn.leaky_relu(net_1, alpha=0.5, name='relu_1')
             net_1 = tf.layers.batch_normalization(net_1, name='batchnorm_1')
             # net_1 = tf.layers.dropout(net_1, 'dropout_1')
-
+            net_1 = tf.layers.conv2d(net_1, 64, 3, 1, padding='same', reuse=tf.AUTO_REUSE, name='conv_1_top')
+            net_1 = tf.nn.leaky_relu(net_1, alpha=0.5, name='relu_1top')
             out = tf.layers.conv2d(net_1, 1, 1, 1, padding='same', reuse=tf.AUTO_REUSE, name='top')
 
         if save_variables:
@@ -369,7 +402,14 @@ class vae_next_gen:
             else:
                 self.sess.run(tf.global_variables_initializer())
 
-            writer = tf.summary.FileWriter('logs/' + model_file + '/', graph=self.sess.graph)
+            all_trainable_vars = tf.reduce_sum([tf.reduce_prod(v.shape) for v in tf.trainable_variables()])
+            print("Model has {0} number of trainable parameters".format(self.sess.run(all_trainable_vars)))
+            writer_1 = tf.summary.FileWriter('logs/' + model_file + '/stack_1/', graph=self.sess.graph, filename_suffix='stack_1')
+            writer_2 = tf.summary.FileWriter('logs/' + model_file + '/stack_2/', graph=self.sess.graph, filename_suffix='stack_2')
+            writer_3 = tf.summary.FileWriter('logs/' + model_file + '/stack_3/', graph=self.sess.graph, filename_suffix='stack_3')
+            writer_4 = tf.summary.FileWriter('logs/' + model_file + '/stack_4/', graph=self.sess.graph, filename_suffix='stack_4')
+            writer = tf.summary.FileWriter('logs/' + model_file + '/full_stack/', graph=self.sess.graph, filename_suffix='full_stack')
+
 
             # summary_disc_1 = tf.summary.merge(self.disc_summaries)
             summary_vae = tf.summary.merge(self.vae_summaries)
@@ -380,11 +420,21 @@ class vae_next_gen:
             for iteration in range(0,iterations+1):
                 self.new_batch = True
 
-                _, vae_loss, merged = self.sess.run([self.vae_train_step_1, self.vae_loss, summary_vae])
-                print('I {:d}. VAE_I {:.6f}'.format(iteration, vae_loss))
-
+                _, vae_loss, merged = self.sess.run([self.vae_train_step, self.vae_loss, summary_vae])
+                print('I {:d}. VAE full-stack {:.6f}'.format(iteration, vae_loss))
+                _, vae_loss_4, merged_4 = self.sess.run([self.vae_train_step_4, self.vae_loss, summary_vae])
+                print('I {:d}. VAE stack 4 {:.6f}'.format(iteration, vae_loss_4))
+                _, vae_loss_3, merged_3 = self.sess.run([self.vae_train_step_3, self.vae_loss, summary_vae])
+                print('I {:d}. VAE stack 3 {:.6f}'.format(iteration, vae_loss_3))
+                _, vae_loss_2, merged_2 = self.sess.run([self.vae_train_step_2, self.vae_loss, summary_vae])
+                print('I {:d}. VAE stack 2 {:.6f}'.format(iteration, vae_loss_2))
+                _, vae_loss_1, merged_1 = self.sess.run([self.vae_train_step_1, self.vae_loss, summary_vae])
+                print('I {:d}. VAE stack 1 {:.6f}'.format(iteration, vae_loss_1))
                 # regularization for early stopping
-                if vae_loss > 1000 or vae_loss < -1000:
+                if (vae_loss > 10000 or vae_loss < -10000) and \
+                        (vae_loss_1 > 10000 or vae_loss_1 < -10000) and \
+                        (vae_loss_2 > 10000 or vae_loss_2 < -10000) and \
+                        (vae_loss_3 > 10000 or vae_loss_3 < -10000):
                     early_stop_counter += 1
                     try:
                         print(["VAE loss to high", "Trying to restore last checkpoint ..."], sep='\n')
@@ -405,6 +455,10 @@ class vae_next_gen:
 
                 if iteration % log_interval == 0:
                     writer.add_summary(merged, global_step=iteration)
+                    writer_1.add_summary(merged_1, global_step=iteration)
+                    writer_2.add_summary(merged_2, global_step=iteration)
+                    writer_3.add_summary(merged_3, global_step=iteration)
+                    writer_4.add_summary(merged_4, global_step=iteration)
                 if iteration % log_interval == 0:
                     if iteration == 0:
                         if not os.path.exists('./images/0'):
@@ -415,6 +469,8 @@ class vae_next_gen:
                             os.makedirs('./images/2')
                         if not os.path.exists('./images/3'):
                             os.makedirs('./images/3')
+                        if not os.path.exists('./images/test_z'):
+                            os.makedirs('./images/test_z')
 
                     batch = self.sess.run(self.dataset_batch)
                     ims = batch[0][:, :, 0]
@@ -437,7 +493,52 @@ class vae_next_gen:
                     imss = ims[int(self.batch_size * 3 / 4)][:, :, 0]
                     plt.imsave('./images/3/' + str(iteration) + '.png', imss, cmap='gray')
 
+                    a = self.sess.run((self.smp1,self.smp2,self.smp3,self.smp4))
+                    zz1 = a[0][2]
+                    zz2 = a[1][2]
+                    zz3 = a[2][2]
+                    zz4 = a[3][2]
 
+                    z1 = np.random.normal(loc=0,scale=1,size=[4*self.n_z])
+                    z4=np.asarray(z1[0:self.n_z]).reshape(1,self.n_z)
+                    z2=np.asarray(z1[self.n_z:2*self.n_z]).reshape(1,self.n_z)
+                    z3=np.asarray(z1[2*self.n_z:3*self.n_z]).reshape(1,self.n_z)
+                    z1=np.asarray(z1[3*self.n_z:4*self.n_z]).reshape(1,self.n_z)
+                    result = self.sess.run(self.dec_out, feed_dict={self.z_1_input:z1,self.z_2_input:z2,self.z_3_input:z3,self.z_4_input:z4,})
+
+                    plt.imsave('./images/test_z/' + str(iteration) + '_random.png',result.reshape(64,64), cmap='gray')
+                    i = np.random.randint(0,self.batch_size)
+                    result = self.sess.run(self.dec_out, feed_dict={self.z_1_input: zz1[i].reshape(1, self.n_z),
+                                                                    self.z_2_input: zz2[i].reshape(1, self.n_z),
+                                                                    self.z_3_input: zz3[i].reshape(1, self.n_z),
+                                                                    self.z_4_input: zz4[i].reshape(1, self.n_z), })
+                    plt.imsave('./images/test_z/' + str(iteration) + '_reconstructed.png', result.reshape(64,64), cmap='gray')
+                    result = self.sess.run(self.dec_out, feed_dict={self.z_1_input: z1.reshape(1, self.n_z),
+                                                                    self.z_2_input: zz2[i].reshape(1, self.n_z),
+                                                                    self.z_3_input: zz3[i].reshape(1, self.n_z),
+                                                                    self.z_4_input: zz4[i].reshape(1, self.n_z), })
+                    plt.imsave('./images/test_z/' + str(iteration) + '_reconstructed_z1.png', result.reshape(64, 64), cmap='gray')
+                    result = self.sess.run(self.dec_out, feed_dict={self.z_1_input: zz1[i].reshape(1, self.n_z),
+                                                                    self.z_2_input: z2.reshape(1, self.n_z),
+                                                                    self.z_3_input: zz3[i].reshape(1, self.n_z),
+                                                                    self.z_4_input: zz4[i].reshape(1, self.n_z), })
+                    plt.imsave('./images/test_z/' + str(iteration) + '_reconstructed_z2.png', result.reshape(64, 64), cmap='gray')
+                    result = self.sess.run(self.dec_out, feed_dict={self.z_1_input: zz1[i].reshape(1, self.n_z),
+                                                                    self.z_2_input: zz2[i].reshape(1, self.n_z),
+                                                                    self.z_3_input: z3.reshape(1, self.n_z),
+                                                                    self.z_4_input: zz4[i].reshape(1, self.n_z), })
+                    plt.imsave('./images/test_z/' + str(iteration) + '_reconstructed_z3.png', result.reshape(64, 64), cmap='gray')
+                    result = self.sess.run(self.dec_out, feed_dict={self.z_1_input: zz1[i].reshape(1, self.n_z),
+                                                                    self.z_2_input: zz2[i].reshape(1, self.n_z),
+                                                                    self.z_3_input: zz3[i].reshape(1, self.n_z),
+                                                                    self.z_4_input: z4.reshape(1, self.n_z), })
+                    plt.imsave('./images/test_z/' + str(iteration) + '_reconstructed_z4.png', result.reshape(64, 64), cmap='gray')
+
+# a = self.sess.run((self.smp1,self.smp2,self.smp3,self.smp4))
+# zz1 = a[0][2]
+# zz2 = a[1][2]
+# zz3 = a[2][2]
+# zz4 = a[3][2]
 
 # z1 = np.random.normal(loc=0,scale=1,size=[80])
 # z4=np.asarray(z1[0:20]).reshape(1,20)
@@ -446,18 +547,11 @@ class vae_next_gen:
 # z1=np.asarray(z1[60:80]).reshape(1,20)
 
 # result = self.sess.run(self.dec_out, feed_dict={self.z_1_input:z1,self.z_2_input:z2,self.z_3_input:z3,self.z_4_input:z4,})
-
-# plt.imshow(result.reshape(64,64),cmap='gray')
-# plt.show()
-
-# i = 0
-# result = self.sess.run(self.dec_out, feed_dict={self.z_1_input:zz1[i].reshape(1,20),self.z_2_input:zz2[0].reshape(1,20),self.z_3_input:z3.reshape(1,20),self.z_4_input:zz4[i].reshape(1,20),})
 # plt.imshow(result.reshape(64,64),cmap='gray')
 # plt.show()
 
 # i = 0
 #
 # result = self.sess.run(self.dec_out, feed_dict={self.z_1_input:zz1[i].reshape(1,20),self.z_2_input:zz2[i].reshape(1,20),self.z_3_input:zz3[i].reshape(1,20),self.z_4_input:zz4[i].reshape(1,20),})
-#
 # plt.imshow(result.reshape(64,64),cmap='gray')
 # plt.show()
--- run.py
+++ run.py
@@ -7,13 +7,15 @@ from statistics import mode
 
 import tensorflow as tf
 # from models.model_GAVAE_SIM import GAVAE_SIM
-from models.model_GAVAE_SIM_tf import GAVAE_SIM
+# from models.model_GAVAE_SIM_tf import GAVAE_SIM
+from models.model_vae20 import vae_next_gen
 
 def main(_):
-    # DATA_PATH = 'D:/Vision_Images/MNIST/mnist.npz'
+    # DATA_PATH = 'D:/Vision_Images/fashion-mnist/'
     DATA_PATH = 'D:/Vision_Images/Pexels_textures/Textures/stonewall'
-    tex_gavae = GAVAE_SIM(data_path=DATA_PATH, w=160, h=160, c=1, layer_depth=3, batch_size=18)
-    tex_gavae.train(iterations=50000, save_interval=500, log_interval=50, model_file='test.h5')
+    # tex_gavae = GAVAE_SIM(data_path=DATA_PATH, w=160, h=160, c=1, layer_depth=3, batch_size=18)
+    tex_vae = vae_next_gen(data_path=DATA_PATH, w=64, h=64, c=1, batch_size=32)
+    tex_vae.train(iterations=100000, save_interval=1000, log_interval=50, model_file='vae_2_0_stonewall_stacked')
     # tex_gavae.transfer_train(iterations=50000, save_interval=100, log_interval=20, model_file='test.h5')
     # tex_gavae.test_discriminant(model_file='test.h5')
     # tex_gavae.test(model_file='test.h5')
